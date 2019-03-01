8db347c86c6981b4ea4afa2e253f238b6188f208
MrCocoaCat <liyubo_liyubo@163.com>
2019-03-01 14:28:33 +0800

    add vxlan function

--- BC/vxlan.py
+++ BC/vxlan.py
@@ -8,6 +8,7 @@ from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
 import ryu.app.ofctl.api
+from ryu.utils import hex_array
 
 # 继承ryu.base.app_manager.RyuApp
 class SimpleSwitch13(app_manager.RyuApp):
@@ -19,8 +20,6 @@ class SimpleSwitch13(app_manager.RyuApp):
         self.sw1 = range(25, 36)
         self.sw2 = range(7, 15)
 
-        self.switchDic = {"0x148bd3d3ad316": "192.168.125.43",
-                          "0x1741f4aa82eef": "192.168.125.47"}
         self._ETH_TYPE_DIC = {0x0800: "TH_TYPE_IP",
                               0x0806: "ETH_TYPE_ARP",
                               0x6558: "ETH_TYPE_TEB",
@@ -33,11 +32,15 @@ class SimpleSwitch13(app_manager.RyuApp):
                               0x88e7: "ETH_TYPE_8021AH",
                               0x05dc: "ETH_TYPE_IEEE802_3",
                               0x8902: "ETH_TYPE_CFM"}
+        self.switchDic = {"0x148bd3d3ad316": "43-instance1",
+                          "0x1741f4aa82eef": "192.168.125.47",
+                          "0x248bd3d3ad316": "43-instance2"
+                          }
         self.dataPathDic = {}
 
     @staticmethod
     def add_flow(datapath, priority, match, actions):
-        print "begin add flow :", match
+        print "begin add flow "
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
         inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
@@ -71,28 +74,21 @@ class SimpleSwitch13(app_manager.RyuApp):
                                     match=match)
             datapath.send_msg(mod)
 
-    def switch1(self, datapath):
-        print "switch1-192.168.125.43"
-        self.clean_flow(datapath, self.sw1)
-        parser = datapath.ofproto_parser
-        # 在iptable 之间加入路由器
-        # 28 为路由 25为iptable
-
-        # 29 为119服务器，33为小交换机
-        match1 = parser.OFPMatch(in_port=29, )
-        actions1 = [parser.OFPActionOutput(33)]
-        match2 = parser.OFPMatch(in_port=33)
-        actions2 = [parser.OFPActionOutput(29)]
-        self.add_flow(datapath, 5, match1, actions1)
-        self.add_flow(datapath, 5, match2, actions2)
-
+    def vxlan(self, datapath):
+        print("vxlan")
+        pass
+        # self.clean_flow(datapath, self.sw1)
+        # parser = datapath.ofproto_parser
+        # match1 = parser.OFPMatch(in_port=18, arp_sha="52:54:00:68:46:03", vlan_vid=600)
+        # #actions1 = [parser.OFPActionOutput(33)]
+        # actions1 = [parser.OFPActionPopVlan()]
+        # self.add_flow(datapath, 5, match1, actions1)
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
         datapath = ev.msg.datapath
         datapath_id = hex(datapath.id)
         self.dataPathDic.setdefault(datapath_id, datapath)
-        print self.dataPathDic
         print "datapath id :%x switch IP:%s" % (datapath.id, self.switchDic[datapath_id])
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
@@ -101,31 +97,29 @@ class SimpleSwitch13(app_manager.RyuApp):
                                           ofproto.OFPCML_NO_BUFFER)]
         self.add_flow(datapath, 0, match, actions)
 
-        if "192.168.125.43" == self.switchDic[datapath_id]:
-            self.switch1(datapath)
+        if "43-instance2" == self.switchDic[datapath_id]:
+            self.vxlan(datapath)
 
 
-    # pack-in
-    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-    def _packet_in_handler(self, ev):
-        if ev.msg.msg_len < ev.msg.total_len:
-            self.logger.debug("packet truncated: only %s of %s bytes",
-                              ev.msg.msg_len, ev.msg.total_len)
+    @set_ev_cls(ofp_event.EventOFPPortStatus, MAIN_DISPATCHER)
+    def port_status_handler(self, ev):
         msg = ev.msg
-        datapath = msg.datapath
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
-        data = msg.data
+        dp = msg.datapath
+        ofp = dp.ofproto
+        if msg.reason == ofp.OFPPR_ADD:
+            reason = 'ADD'
+        elif msg.reason == ofp.OFPPR_DELETE:
+            reason = 'DELETE'
+        elif msg.reason == ofp.OFPPR_MODIFY:
+            reason = 'MODIFY'
+        else:
+            reason = 'unknown'
+        print('OFPPortStatus received: reason=%s desc=%s', reason, msg.desc)
 
-        in_port = msg.match['in_port']
-        pkt = packet.Packet(data)
-        eth = pkt.get_protocols(ethernet.ethernet)[0]
-        # eth 为class ethernet 对象
-        dst = eth.dst
-        src = eth.src
-        ethertype = self._ETH_TYPE_DIC.get(eth.ethertype, "UNKNOW_TYPE")
-        datapath_id = hex(datapath.id)
-        self.logger.info("packet in %s -- ethertype:%s src:%s dst:%s in_port:%s ",
-                         datapath_id, ethertype, src, dst, in_port)
 
+    @set_ev_cls(ofp_event.EventOFPErrorMsg,[ CONFIG_DISPATCHER, MAIN_DISPATCHER])
+    def error_msg_handler(self, ev):
+        msg = ev.msg
+        print('OFPErrorMsg received: type=0x%02x code=0x%02x '
+                'message=%s', msg.type, msg.code, hex_array(msg.data))
 
